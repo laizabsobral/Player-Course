423e4119860fb61128eb6c744e949d02954b56d3
laizabsobral <laiza.b.sobral@accenture.com>
2023-09-18 19:53:45 -0300

    organization folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Provider as ReduxProvider } from "react-redux";
 
-import { TodoList } from "./components/TodoList";
-import { AddTodo } from "./components/addTodo";
 import { store } from "./store";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+     
     </ReduxProvider>
   );
 }
