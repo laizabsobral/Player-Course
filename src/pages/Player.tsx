9a75b6b2bb93261812e7b8b7019c0c8fd965b864
laizabsobral <laiza.b.sobral@accenture.com>
2023-09-18 20:56:57 -0300

    add props modules

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -22,9 +22,9 @@ export function Player() {
             <Video/>
           </div>
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            <Module/>
-            <Module/>
-            <Module/>
+            <Module moduleIndex={0} title="Desvendando o Redux" amountOfLesson={3}/>
+            <Module moduleIndex={1} title="Desvendando o Redux" amountOfLesson={3}/>
+            <Module moduleIndex={2} title="Desvendando o Redux" amountOfLesson={3}/>
           </aside>
         </main>
       </div>
